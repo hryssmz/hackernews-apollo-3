43dd872e6534971879201997e4bc941000c3bc47
Hiroyasu Shimizu <hryssmz@yahoo.com>
2022-09-20 00:43:06 +0900

    Add some filters and paginations for feed API

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,14 +1,45 @@
 // resolvers/Query.ts
 import prisma from "../utils/prisma";
-import type { Link } from "@prisma/client";
+import type { Link, Prisma } from "@prisma/client";
 
 function info(): string {
   return "This is the API of a Hackernews Clone";
 }
 
-async function feed(): Promise<Link[]> {
-  const links = await prisma.link.findMany();
-  return links;
+interface LinkOrderBy {
+  description?: Prisma.SortOrder;
+  url?: Prisma.SortOrder;
+  createdAt?: Prisma.SortOrder;
+}
+
+interface Feed {
+  links: Link[];
+  count: number;
+}
+
+async function feed(
+  _: unknown,
+  args: { filter?: string; skip?: number; take?: number; orderBy?: LinkOrderBy }
+): Promise<Feed> {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = await prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  const count = await prisma.link.count({ where });
+
+  return { links, count };
 }
 
 async function link(_: unknown, args: Pick<Link, "id">): Promise<Link | null> {
